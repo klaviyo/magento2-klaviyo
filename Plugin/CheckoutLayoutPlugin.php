e89155e5aceb25623fe9e5e09d049b0770568d27
klaviyojad <jad.atoui@klaviyo.com>
2021-09-20 18:53:36 -0400

    code cleanup

--- Plugin/CheckoutLayoutPlugin.php
+++ Plugin/CheckoutLayoutPlugin.php
@@ -14,10 +14,36 @@ class CheckoutLayoutPlugin
         $this->_klaviyoScopeSetting = $klaviyoScopeSetting;
     }
 
+    /**
+     * Checks if logged in user has a default address set, if not returns false.
+     *
+     * @param Magento\Framework\App\ObjectManager $objectManager
+     * @param Magento\Customer\Model\Session $customerSession
+     *
+     * @return Magento\Customer\Model\Address|false
+     */
+    public function _getDefaultAddressIfSetForCustomer(
+        \Magento\Framework\App\ObjectManager $objectManager,
+        \Magento\Customer\Model\Session $customerSession
+    ) {
+        $address = false;
+        if ($customerSession->isLoggedIn()) {
+            $customerData = $customerSession->getCustomer()->getData();
+            $customerFactory = $objectManager->get('\Magento\Customer\Model\CustomerFactory')->create();
+            $customerId = $customerData["entity_id"];
+            $customer = $customerFactory->load($customerId);
+            $address = $customer->getDefaultShippingAddress();
+        }
+        return $address;
+    }
+
     public function afterProcess(\Magento\Checkout\Block\Checkout\LayoutProcessor $processor, $jsLayout)
     {
-        if ($this->_klaviyoScopeSetting->getConsentAtCheckoutSMSIsActive())
-        {
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $customerSession = $objectManager->get('Magento\Customer\Model\Session');
+
+        if ($this->_klaviyoScopeSetting->getConsentAtCheckoutSMSIsActive()) {
+
             $smsConsentCheckbox = [
                 'component' => 'Magento_Ui/js/form/element/abstract',
                 'config' => [
@@ -38,12 +64,33 @@ class CheckoutLayoutPlugin
                 'id' => 'kl_sms_consent',
             ];
 
-            $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']['shippingAddress']['children']['shipping-address-fieldset']['children']['kl_sms_consent'] = $smsConsentCheckbox;
+            $address = $this->_getDefaultAddressIfSetForCustomer($objectManager, $customerSession);
+
+            if (!$address)
+                $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']['shippingAddress']['children']['shipping-address-fieldset']['children']['kl_sms_consent'] = $smsConsentCheckbox;
+            else {
+
+                // extra un-editable field with saved phone number to display to logged in users with default address set
+                $smsConsentTelephone = [
+                    'component' => 'Magento_Ui/js/form/element/abstract',
+                    'config' =>
+                        [
+                            'customScope' => 'shippingAddress',
+                            'template' => 'ui/form/field',
+                            'elementTmpl' => 'ui/form/element/input',
+                        ],
+                    'label' => 'Phone Number',
+                    'provider' => 'checkoutProvider',
+                    'sortOrder' => '120',
+                    'disabled' => true,
+                    'visible' => true,
+                    'value' => $address->getTelephone()
+                ];
+
+                $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']['shippingAddress']['children']['before-form']['children']['kl_sms_phone_number'] = $smsConsentTelephone;
+                $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']['shippingAddress']['children']['before-form']['children']['kl_sms_consent'] = $smsConsentCheckbox;
+            }
         }
-        // Open to ideas here, since we don't overwrite the customer-email section
-        // we need to distinguish if the customer is logged in or not, object manager is an easy way to do so
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $customerSession = $objectManager->get('Magento\Customer\Model\Session');
 
         if (!$customerSession->isLoggedIn() && $this->_klaviyoScopeSetting->getConsentAtCheckoutEmailIsActive())
         {
