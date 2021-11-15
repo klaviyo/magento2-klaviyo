67d5e2d2151c6ba4ce928855bae7e63caaf51c77
jordan allain <jordan.allain@klaviyo.com>
2021-11-15 14:54:54 -0500

    Interpolate with double quotes

--- Helper/Webhook.php
+++ Helper/Webhook.php
@@ -65,7 +65,7 @@ class Webhook extends \Magento\Framework\App\Helper\AbstractHelper
         $err = curl_errno($curl);
 
         if ($err) {
-            $this->_klaviyoLogger->log(sprintf("Unable to send webhook to %s with data: %s", $url, $data));
+            $this->_klaviyoLogger->log(sprintf("Unable to send webhook to $url with data: $data"));
         }
 
         // Close cURL session handle
