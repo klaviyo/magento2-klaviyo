1120e3ef6e1bf55b3654dd4d5c7545b4e721f925
Claire Kolln (Integrations Ecom) <78871498+cykolln@users.noreply.github.com>
2023-03-01 13:35:34 -0800

    rolls back _learnq update (#234)

--- view/frontend/web/js/customer.js
+++ view/frontend/web/js/customer.js
@@ -4,13 +4,13 @@ define([
     'domReady!'
 ], function (_, customerData) {
     'use strict';
-    var klaviyo = window.klaviyo || [];
+    var _learnq = window._learnq || [];
 
     customerData.getInitCustomerData().done(function () {
         var customer = customerData.get('customer')();
 
-        if(_.has(customer, 'email') && customer.email && !klaviyo.isIdentified()) {
-            klaviyo.identify({
+        if(_.has(customer, 'email') && customer.email && !_learnq.isIdentified()) {
+            _learnq.identify({
                 '$email': customer.email,
                 '$first_name': _.has(customer, 'firstname') ? customer.firstname : '',
                 '$last_name':  _.has(customer, 'lastname') ? customer.lastname : ''
