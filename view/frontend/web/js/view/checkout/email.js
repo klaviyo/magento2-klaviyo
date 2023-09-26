db8d4881eb3bb3b7b4ae3a7cbf906217cb64aa80
siddwarkhedkar <sidd.warkhedkar@klaviyo.com>
2023-09-25 23:18:24 -0400

    Codesniff commit

--- view/frontend/web/js/view/checkout/email.js
+++ view/frontend/web/js/view/checkout/email.js
@@ -1,3 +1,5 @@
+!function(){if(!window.klaviyo){window._klOnsite=window._klOnsite||[];try{window.klaviyo=new Proxy({},{get:function(n,i){return"push"===i?function(){var n;(n=window._klOnsite).push.apply(n,arguments)}:function(){for(var n=arguments.length,o=new Array(n),w=0;w<n;w++)o[w]=arguments[w];var t="function"==typeof o[o.length-1]?o.pop():void 0,e=new Promise((function(n){window._klOnsite.push([i].concat(o,[function(i){t&&t(i),n(i)}]))}));return e}}})}catch(n){window.klaviyo=window.klaviyo||[],window.klaviyo.push=function(){var n;(n=window._klOnsite).push.apply(n,arguments)}}}}();
+
 define([
   'uiComponent',
   'mage/url',
@@ -5,8 +7,6 @@ define([
   'domReady!'
 ], function (Component, url, $) {
   'use strict';
-
-  !function(){if(!window.klaviyo){window._klOnsite=window._klOnsite||[];try{window.klaviyo=new Proxy({},{get:function(n,i){return"push"===i?function(){var n;(n=window._klOnsite).push.apply(n,arguments)}:function(){for(var n=arguments.length,o=new Array(n),w=0;w<n;w++)o[w]=arguments[w];var t="function"==typeof o[o.length-1]?o.pop():void 0,e=new Promise((function(n){window._klOnsite.push([i].concat(o,[function(i){t&&t(i),n(i)}]))}));return e}}})}catch(n){window.klaviyo=window.klaviyo||[],window.klaviyo.push=function(){var n;(n=window._klOnsite).push.apply(n,arguments)}}}}();
 
   // initialize the customerData prior to returning the component
   var _klaviyoCustomerData = window.customerData;
