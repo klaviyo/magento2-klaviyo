33098f14f476f6d4eff9aa639d5d4dd60a2f825c
Jordan Leslie <jordan.leslie@klaviyo.com>
2023-10-02 11:14:17 -0500

    Remove exit statement
    This is level 10 security risk according to Magento and will not pass its tests

--- KlaviyoV3Sdk/Exception/index.php
+++ KlaviyoV3Sdk/Exception/index.php
@@ -6,4 +6,3 @@ header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache');
 header('Location: ../');
-exit;
